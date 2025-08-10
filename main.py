e1d6f1d4d591054d620c4213ab9b48a1370dd2eb
Ahmed Rezk <work72019@gmail.com>
2025-08-09 20:17:26 -0600

    ✨ (main.py, .gitignore, .python-version, README.md, pyproject.toml): add initial Python project structure and dependencies for a simple AI chat application using Ollama API

---
+++ main.py
@@ -0,0 +1,56 @@
+from ollama import ChatResponse, Client
+from IPython.display import display, Markdown
+import json
+
+client = Client(
+    host="http://192.168.50.252:11434", headers={"x-some-header": "some-value"}
+)
+
+
+friends_list = ["Tommy", "Isabel", "Daniel"]
+list_of_tasks = [
+    "Compose a brief email to my boss explaining that I will be late for tomorrow's meeting.",
+    "Write a birthday poem for Otto, celebrating his 28th birthday.",
+    "Write a 300-word review of the movie 'The Arrival'.",
+]
+
+ice_cream_flavors = ["Chocolate", "Mint Chocolate Chip"]
+
+food_preferences_tommy = {
+    "dietary_restrictions": "vegetarian",
+    "favorite_ingredients": ["tofu", "olives"],
+    "experience_level": "intermediate",
+    "maximum_spice_level": 6,
+}
+
+prompt = f"""Please suggest a rescipe taht tries to include the following ingredients: {food_preferences_tommy["favorite_ingredients"]}
+The recipe should adhere to the following dietary restrictions: {food_preferences_tommy["dietary_restrictions"]}
+The difficulty of the recipe should be: {food_preferences_tommy["experience_level"]}
+The maximum spice level on a scale of 10 should be: {food_preferences_tommy["maximum_spice_level"]}
+Provide a two sentence description
+"""
+
+response: ChatResponse = client.chat(
+    model="llama3:latest",
+    messages=[
+        {
+            "role": "user",
+            "content": prompt,
+        },
+    ],
+)
+
+# Reading Markdown
+file = open("./baking.md")
+recipe = file.read()
+# __AUTO_GENERATED_PRINT_VAR_START__
+print(f" recipe: {display(Markdown(recipe))}")  # __AUTO_GENERATED_PRINT_VAR_END__
+file.close()
+
+
+def main():
+    print("Hello from ai-python!")
+
+
+if __name__ == "__main__":
+    main()
